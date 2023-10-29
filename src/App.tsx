c59d01e1ef6582d1001ff1ac3724b12e87dc0aaf
mitrich <4irtim@gmail.com>
2023-10-29 10:29:39 +0300

    feat: add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import { Component, ReactNode } from 'react';
 
 export default class App extends Component {
   render(): ReactNode {
-    return <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello, {'lol'}!</h1>;
+    return (
+      <main className="flex flex-col justify-top h-screen bg-gray-700 font-mono">
+        <nav className='h-1/6 flex flex-row content-start gap-10'>
+          <h1 className="text-2xl font-bold text-red-400 text-left">
+            RSS Astro Objects
+          </h1>
+        </nav>
+        <section className='h-auto'>
+          CONTENT
+        </section>
+      </main>
+    );
   }
 }
