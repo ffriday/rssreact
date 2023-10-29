cf5eff1bba68e805fab617020b7814cb14af84fb
mitrich <4irtim@gmail.com>
2023-10-29 09:54:27 +0300

    feat: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import { Component, ReactNode } from 'react';
 
 export default class App extends Component {
   render(): ReactNode {
-    return <h1>Hello, {'lol'}!</h1>;
+    return <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello, {'lol'}!</h1>;
   }
 }
