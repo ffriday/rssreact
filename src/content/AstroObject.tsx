884c5e4a73a92151d64b8bc2bda128f9db5400f0
mitrich <4irtim@gmail.com>
2023-11-01 16:21:40 +0300

    refactor: AstroObject component with hooks

--- src/content/AstroObject.tsx
+++ src/content/AstroObject.tsx
@@ -1,20 +1,15 @@
-import { Component, ReactNode } from 'react';
 import { TAstromicalObject } from '../constants/types';
 
-export default class AstroObject extends Component<TAstromicalObject> {
-  constructor(props: TAstromicalObject) {
-    super(props);
-  }
-
-  render(): ReactNode {
-    return (
-      <li className="flex flex-col bg-gray-600 font-mon pl-2 text-white">
-        <h3>Name: {this.props.name}</h3>
-        <p>Object type: {this.props.astronomicalObjectType}</p>
-        <p>
-          Location: {this.props.location ? this.props.location.name : 'Unknown'}
-        </p>
-      </li>
-    );
-  }
+export default function AstroObject({
+  astronomicalObjectType,
+  name,
+  location,
+}: TAstromicalObject): JSX.Element {
+  return (
+    <li className="flex flex-col bg-gray-600 font-mon pl-2 text-white">
+      <h3>Name: {name}</h3>
+      <p>Object type: {astronomicalObjectType}</p>
+      <p>Location: {location ? location.name : 'Unknown'}</p>
+    </li>
+  );
 }
