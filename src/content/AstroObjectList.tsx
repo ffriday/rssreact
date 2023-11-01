d5be991943f4db2f34012d62f18aa39e46b7a065
mitrich <4irtim@gmail.com>
2023-11-01 16:23:10 +0300

    refactor: AstroObjectList component with hooks

--- src/content/AstroObjectList.tsx
+++ src/content/AstroObjectList.tsx
@@ -1,19 +1,14 @@
-import { Component, ReactNode } from 'react';
 import AstroObject from './AstroObject';
 import { TAstroObjectList } from '../constants/types';
 
-export default class AstroObjectList extends Component<TAstroObjectList> {
-  constructor(props: TAstroObjectList) {
-    super(props);
-  }
-
-  render(): ReactNode {
-    return (
-      <ul className="flex flex-col gap-1 px-2">
-        {this.props.AstromicalObject.map((element) => (
-          <AstroObject key={element.uid} {...element} />
-        ))}
-      </ul>
-    );
-  }
+export default function AstroObjectList({
+  AstromicalObject,
+}: TAstroObjectList): JSX.Element {
+  return (
+    <ul className="flex flex-col gap-1 px-2">
+      {AstromicalObject.map((element) => (
+        <AstroObject key={element.uid} {...element} />
+      ))}
+    </ul>
+  );
 }
