46b19e1f7dbf50b3148b2b43666f039edafb4937
mitrich <4irtim@gmail.com>
2023-11-01 21:48:54 +0300

    refactor: App component with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,78 @@
-import { Component, ReactNode } from 'react';
+import { useState, useEffect } from 'react';
 import Search from './search/Search';
-import { apiEnv } from './constants/env';
 import AstroObjectList from './content/AstroObjectList';
-import {
-  TAstroObjectList,
-  TAstromicalObject,
-  TColor,
-  TError,
-  TErrorInfo,
-} from './constants/types';
-import { loadLastSearch } from './helpers/helpers';
+import { TAstromicalObject, TColor, TErrorInfo } from './constants/types';
+import { apiSearchRequest, loadLastSearch } from './helpers/helpers';
 import ErrorBox from './errorBox/error';
 
 type TBodyResponse = {
   astronomicalObjects: TAstromicalObject[];
 };
 
-type TAppState = TAstroObjectList &
-  TError & {
-    loading: boolean;
-  };
+export default function App(): JSX.Element {
+  const lastSearch = loadLastSearch();
 
-export default class App extends Component {
-  private lastSearch = loadLastSearch();
+  const [AstromicalObject, setAstromicalObject] = useState<TAstromicalObject[]>(
+    []
+  );
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  request = async (name: string, pageSize: number) => {
-    return await fetch(
-      `${apiEnv.url}${apiEnv.endpoint}?name=${name}&pageSize=${pageSize}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
-    );
-  };
+  useEffect(() => {
+    (async () => await search(lastSearch))();
+  }, [lastSearch]);
 
-  componentDidMount() {
-    this.search(this.lastSearch);
-  }
-
-  search = async (value: string) => {
-    this.setState({ error: '' });
-    this.setState({ loading: true });
+  const search = async (value: string) => {
+    console.log('search');
+    setError('');
+    setLoading(true);
     try {
-      const res = await this.request(value, 50);
+      const res = await apiSearchRequest(value, 50);
       if (res.ok) {
         const { astronomicalObjects }: TBodyResponse = await res.json();
-        this.setState({ AstromicalObject: astronomicalObjects });
-        if (!astronomicalObjects.length)
-          this.setState({ error: TErrorInfo.empty });
+        setAstromicalObject(astronomicalObjects);
+        if (!astronomicalObjects.length) setError(TErrorInfo.empty);
       }
     } catch (error) {
-      if (error instanceof Error) this.setState({ error: error.message });
+      if (error instanceof Error) setError(error.message);
     }
-    this.setState({ loading: false });
+    setLoading(false);
   };
 
-  fakeError = () => {
+  const fakeError = () => {
     try {
       {
         throw new Error('Test error');
       }
     } catch (error) {
-      if (error instanceof Error) this.setState({ error: error.message });
+      if (error instanceof Error) setError(error.message);
     }
   };
 
-  state: Readonly<TAppState> = {
-    AstromicalObject: [],
-    error: '',
-    loading: false,
-  };
+  if (error && error !== TErrorInfo.empty) throw new Error('Test error');
 
-  render(): ReactNode {
-    if (this.state.error && this.state.error !== TErrorInfo.empty)
-      throw new Error('Test error');
-    return (
-      <main className="flex flex-col justify-top h-full bg-gray-700 font-mono">
-        <nav className="h-15 sm:h-10 flex flex-row flex-wrap content-center justify-start pb-2">
-          <h1 className="text-xl sm:text-2xl w-screen sm:w-max sm:pr-10 font-bold text-red-400 text-left">
-            RSS Astro Objects
-          </h1>
-          <Search searchHandler={this.search} defaultValue={this.lastSearch} />
-          <button
-            onClick={this.fakeError}
-            className="flex h-6 sm:h-7 content-center justify-center flex-wrap bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-5 mt-1"
-          >
-            Error
-          </button>
-        </nav>
-        <section className="h-auto bg-gray-700">
-          {this.state.loading && (
-            <ErrorBox error="Loading..." color={TColor.lime} />
-          )}
-          {this.state.error ? (
-            <ErrorBox error={this.state.error} color={TColor.red} />
-          ) : (
-            <AstroObjectList AstromicalObject={this.state.AstromicalObject} />
-          )}
-        </section>
-      </main>
-    );
-  }
+  return (
+    <main className="flex flex-col justify-top h-full bg-gray-700 font-mono">
+      <nav className="h-15 sm:h-10 flex flex-row flex-wrap content-center justify-start pb-2">
+        <h1 className="text-xl sm:text-2xl w-screen sm:w-max sm:pr-10 font-bold text-red-400 text-left">
+          RSS Astro Objects
+        </h1>
+        <Search searchHandler={search} defaultValue={lastSearch} />
+        <button
+          onClick={fakeError}
+          className="flex h-6 sm:h-7 content-center justify-center flex-wrap bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-5 mt-1"
+        >
+          Error
+        </button>
+      </nav>
+      <section className="h-auto bg-gray-700">
+        {loading && <ErrorBox error="Loading..." color={TColor.lime} />}
+        {error ? (
+          <ErrorBox error={error} color={TColor.red} />
+        ) : (
+          <AstroObjectList AstromicalObject={AstromicalObject} />
+        )}
+      </section>
+    </main>
+  );
 }
--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,4 +1,17 @@
+import { apiEnv } from '../constants/env';
 import { LSKey } from '../constants/types';
 
 export const loadLastSearch = (): string =>
   window.localStorage.getItem(LSKey.lastSearch) ?? '';
+
+export const apiSearchRequest = async (name: string, pageSize: number) => {
+  return await fetch(
+    `${apiEnv.url}${apiEnv.endpoint}?name=${name}&pageSize=${pageSize}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    }
+  );
+};
