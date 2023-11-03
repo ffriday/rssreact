14e2dbb90a9c9aef2550493fbb823accec011b72
mitrich <4irtim@gmail.com>
2023-11-03 22:43:10 +0300

    feat: add pagination

--- src/pagination/pagination.tsx
+++ src/pagination/pagination.tsx
@@ -1,7 +1,33 @@
-import { TSearchPage } from '../constants/types';
+import { Link, useParams, useSearchParams } from 'react-router-dom';
+import { QueryParams, TSearchPage } from '../constants/types';
 
 export default function Pagination({
-  numberOfElements,
+  firstPage,
+  lastPage,
+  pageNumber,
 }: TSearchPage): JSX.Element {
-  return <>{numberOfElements}</>;
+  const { query } = useParams();
+  const [searchParams] = useSearchParams();
+  const uid = searchParams.get(QueryParams.uid);
+  const params = uid ? `?${QueryParams.uid}=${uid}` : '';
+  const back = firstPage ? pageNumber : pageNumber - 1;
+  const forward = lastPage ? pageNumber : pageNumber + 1;
+
+  return (
+    <nav className="flex flex-row text-white p-2 items-center justify-center">
+      <Link
+        className={`p-2 select-none ${firstPage ? 'pointer-events-none' : ''}`}
+        to={`/${back}/${query ?? ''}${params}`}
+      >
+        {'<-'}
+      </Link>
+      <p className="p-2">{pageNumber + 1}</p>
+      <Link
+        className={`p-2 select-none ${lastPage ? 'pointer-events-none' : ''}`}
+        to={`/${forward}/${query ?? ''}${params}`}
+      >
+        {'->'}
+      </Link>
+    </nav>
+  );
 }
