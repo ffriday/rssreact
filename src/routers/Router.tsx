e0a7d952aeefb7b5aff161d3fef511ca93104193
mitrich <4irtim@gmail.com>
2023-11-03 17:43:02 +0300

    fix: initial loading previous search

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -6,14 +6,22 @@ import {
 } from 'react-router-dom';
 import RootLayout from '../layouts/RootLayout';
 import { apiLoadData } from '../helpers/helpers';
-import { QueryParams } from '../constants/types';
+import { LSKey, QueryParams } from '../constants/types';
 import ErrorMessage from '../errorBoundary/errorMessage';
 import Content from '../content/Content';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />} errorElement={<ErrorMessage />}>
-      <Route index element={<Navigate to="/0" />} errorElement={<Content />} />
+      <Route
+        index
+        element={
+          <Navigate
+            to={`/0/${window.localStorage.getItem(LSKey.lastSearch) || ''}`}
+          />
+        }
+        errorElement={<Content />}
+      />
       <Route
         path={`/:${QueryParams.pageNumber}/:${QueryParams.query}?/:${QueryParams.uid}?`}
         element={<Content />}
