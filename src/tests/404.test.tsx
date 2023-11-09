d0d1c97afd39edd6e48806e8320fe0d425e1dc97
mitrich <4irtim@gmail.com>
2023-11-09 16:40:22 +0300

    feat: add 404 test

---
+++ src/tests/404.test.tsx
@@ -0,0 +1,22 @@
+import { act, render, screen } from '@testing-library/react';
+import { RouterProvider } from 'react-router-dom';
+import { expect, it } from 'vitest';
+import ErrorBoundary from '../errorBoundary/ErrorBoundary';
+import { router } from '../routers/Router';
+import { TErrorInfo } from '../constants/types';
+
+const wrongRoute = `${import.meta.env.BASE_URL}0/test/wrongRoute`;
+
+it('Renders 404 page', async () => {
+  render(
+    <ErrorBoundary>
+      <RouterProvider router={router} />
+    </ErrorBoundary>
+  );
+
+  await act(async () => router.navigate(wrongRoute));
+
+  const element = screen.queryByText(TErrorInfo.notFound);
+
+  expect(element).not.toBeNull();
+});
