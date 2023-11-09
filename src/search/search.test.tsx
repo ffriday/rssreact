76a82d6c420cd6286d723113847b42c0aee82bf2
mitrich <4irtim@gmail.com>
2023-11-09 15:52:08 +0300

    feat: add search component tests

--- src/search/search.test.tsx
+++ src/search/search.test.tsx
@@ -1,18 +1,46 @@
-import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Search from './Search';
 import { BrowserRouter } from 'react-router-dom';
+import { LSKey } from '../constants/types';
+import { loadLastSearch } from '../helpers/helpers';
+
+const valueForInput = 'test-text';
+const valueForLS = `second-${valueForInput}`;
 
 describe('Search component', () => {
-  it('Renders correcctly', () => {
+  beforeEach(() => {
+    window.localStorage.setItem(LSKey.lastSearch, valueForLS);
     render(
       <BrowserRouter>
-        <Search defaultValue="" />
+        <Search defaultValue={loadLastSearch()} />
       </BrowserRouter>
     );
-    const searchButton = screen.queryByText('Search');
-    const input = screen.findByPlaceholderText('Type something');
+  });
+
+  it('Renders', () => {
+    const searchButton = screen.getByRole('button');
+    const input = screen.getByRole('searchbox');
+
     expect(searchButton).not.toBeNull();
     expect(input).not.toBeNull();
+  });
+
+  it('Loads value from LS', () => {
+    expect(screen.queryByDisplayValue(valueForLS)).not.toBeNull();
+  });
+
+  it('Saves value to LS', () => {
+    const searchButton = screen.getByRole('button');
+    const input = screen.getByRole('searchbox');
+
+    fireEvent.change(input, { target: { value: valueForInput } });
+    fireEvent.click(searchButton);
+
+    expect(screen.queryByDisplayValue(valueForInput)).not.toBeNull();
+    expect(screen.queryByDisplayValue(valueForLS)).toBeNull();
+    expect(
+      window.localStorage.getItem(LSKey.lastSearch) === valueForInput
+    ).toBeTruthy();
   });
 });
