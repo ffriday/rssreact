9636cb19841d69bdaef4d3b56bd985d0c096406b
mitrich <4irtim@gmail.com>
2023-11-09 13:05:57 +0300

    feat: add vitest, react-testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+    },
+  },
   plugins: [react()],
 });
